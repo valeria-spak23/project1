ea202e9b15830930bfcdd82d5402423565d5b23b
Valeria Spak <valeriaspak23@gmail.com>
2025-04-08 21:08:29 +0300

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by Valeria 🤯🤪 on 08/04/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
